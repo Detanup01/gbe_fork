6304b36ed408a9ddebea97098a13e209d1288472
Simon Leary <simon.leary42@proton.me>
2026-01-20 18:27:20 -0500

    wip

--- dll/settings_parser_ufs.cpp
+++ dll/settings_parser_ufs.cpp
@@ -268,16 +268,21 @@ void parse_cloud_save(CSimpleIniA *ini, class Settings *settings_client, class S
         auto default_cloud_dir = factory_default_cloud_dir(ini, settings_client, settings_server, local_storage);
         if (default_cloud_dir.empty()) {
             PRINT_DEBUG("[X] cannot resolve default cloud save dir");
-        } else if (std::filesystem::is_directory(default_cloud_dir) || std::filesystem::create_directories(default_cloud_dir)) {
-            PRINT_DEBUG(
-                "successfully created default cloud save dir '%s'",
-                default_cloud_dir.u8string().c_str()
-            );
-        } else {
-            PRINT_DEBUG(
-                "[X] failed to create default cloud save dir '%s'",
-                default_cloud_dir.u8string().c_str()
-            );
+        } else if (!std::filesystem::is_directory(default_cloud_dir)) {
+            std::error_code ec;
+            if (std::filesystem::create_directories(default_cloud_dir, ec)) {
+                PRINT_DEBUG(
+                    "successfully created default cloud save dir '%s'",
+                    default_cloud_dir.u8string().c_str()
+                );
+            } else {
+                PRINT_DEBUG(
+                    "[X] failed to create default cloud save dir '%s': '%s'",
+                    default_cloud_dir.u8string().c_str(),
+                    ec.message().c_str()
+                );
+                // TODO crash the program here, or just print and move on?
+            }
         }
     }
 
