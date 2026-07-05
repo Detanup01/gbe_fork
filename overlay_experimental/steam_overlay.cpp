be257f3de9ab6ef5cda1857fbc287f8e720a0f96
Piezometric <ugurxkahriman@gmail.com>
2026-07-05 15:10:32 +0300

    Fix screenshot notification submission logic

--- overlay_experimental/steam_overlay.cpp
+++ overlay_experimental/steam_overlay.cpp
@@ -3014,8 +3014,13 @@ void Steam_Overlay::process_captured_screenshots()
         if (local_storage->save_screenshot(filename, item.pixels_rgb.data(), item.width, item.height, 4)) {
             PRINT_DEBUG("Screenshot saved: %s", filename.c_str());
             submit_notification(
-                notification_type::screenshot, 
-                std::string(translationScreenshotSaved[current_language]) + settings->overlay_appearance.screenshot_datetime_format.c_str()
+                notification_type::screenshot,
+    #ifdef _MSC_VER
+            localtime_s(&local_tm, &now_time);
+    #else
+            localtime_r(&now_time, &local_tm);
+    #endif
+                std::string(translationScreenshotSaved[current_language]) + settings->overlay_appearance.screenshot_datetime_format.c_str(), &local_tm
             );
         } else {
             PRINT_DEBUG("Failed to save screenshot!");
