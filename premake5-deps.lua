90747478e0d3cd8ddc3bd88fa11b04a1d9677e7f
Detanup01 <91248446+Detanup01@users.noreply.github.com>
2026-09-21 21:22:00 +0200

    cant believe it

--- premake5-deps.lua
+++ premake5-deps.lua
@@ -9,10 +9,10 @@ require("premake", ">=5.0.0-beta8")
 -- MSYS Makefiles
 
 local os_iden = '' -- identifier
-print(os.hostarch())
+local is_arm = os.hostarch() == "AARCH64" or os.hostarch() == "ARM64"
 if os.target() == "windows" then
     os_iden = 'win'
-elseif os.target() == "linux" and (os.hostarch() == "AARCH64" or os.hostarch() == "ARM64") then
+elseif os.target() == "linux" and is_arm then
     os_iden = "linux-arm"
 elseif os.target() == "linux" then
     os_iden = 'linux'
